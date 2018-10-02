2e22895006c2820b7b8dfc23e8731df0f141bc3f
PsySc0rpi0n <totallacc@gmail.com>
2018-10-02 23:09:37 +0100

    File readiness check added

--- tcplay.sh
+++ tcplay.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-echo $#
-if [ $# -lt 2 ]; then
-   echo "Not enough parameters. Usage: " $0 "path/to/file/filename.ext"
-else
-   echo "File" $1 "found!"
+if [ $# -lt 1 ]; then
+   echo "Not enough parameters. Usage: $0 path/to/file/filename.ext"
+fi
+
+if [[ ! -e $1 || ! -r $1 || ! -f $1 ]]; then
+   echo "File $1 not found or ready!"
 fi
