af726797eddc27ad9d00f76da258f43c3faa1102
PsySc0rpi0n <totallacc@gmail.com>
2018-10-03 00:05:07 +0100

    losetup first routine attempted

--- tcplay.sh
+++ tcplay.sh
@@ -7,3 +7,10 @@ fi
 if [[ ! -e $1 || ! -r $1 || ! -f $1 ]]; then
    echo "File $1 not found or ready!"
 fi
+
+export MOUNT=/dev/loop0
+if grep -qs $MOUNT /proc/mounts ; then
+   echo "loop0 device is ready! Command status: $?"
+else
+   echo "$MOUNT is not ready!"
+fi
