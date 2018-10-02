2e823d84fc0d61a38c82ba950badff16504fe7cb
PsySc0rpi0n <totallacc@gmail.com>
2018-10-02 21:36:33 +0100

    Initial commit!

---
+++ tcplay.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Enter Virtual Volume filename and path:"
+read filename
+expfilename=$filename
+echo "$expfilename"
+
+if [ -s $filename ]; then
+    echo "$filename exists!"
+
+    if [ -r $filename ]; then
+        echo "$filename is readable!"
+    fi
+
+    if [ -f $filename ]; then
+        echo "$filename is accessible!"
+    fi
+else
+    echo "$filename not found!"
+fi
