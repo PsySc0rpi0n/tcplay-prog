3b3230a961c33811b0fca55a7ac7e63cf4c7875c
PsySc0rpi0n <totallacc@gmail.com>
2019-05-12 12:40:17 +0100

    Last version

--- tcplay.sh
+++ tcplay.sh
@@ -1,26 +1,56 @@
 #!/bin/sh
 
-if [ $# -lt 1 ]; then
-   echo "Not enough parameters. Usage: $0 path/to/file/filename.ext"
-   exit 1
-fi
+read -ep "Make Available [a] or Make Unavailable [u]: " opt
 
-if [[ ! -e $1 || ! -r $1 || ! -f $1 ]]; then
-   echo "File $1 not found or not ready!"
-   exit 1
-fi
+while [[ $opt -ne 'a' && $opt -ne 'u' && $opt -ne 'x' ]]
+do
+   echo "Wrong option!"
+   echo "Available options are [a], [u] or [x] to quit!"
+done
 
-sudo losetup /dev/loop0 $1
-if [[ $? -ne 0 ]]; then
-   echo "Error with $1!" >&2
-   exit 1
-fi
+echo $opt
 
-sudo tcplay -m $1 -d /dev/loop0
-if [[ $? -ne 0]]; then
-   echo "Error attaching $1 container to /dev/loop0!" >&2
-   exit 1
-fi
+case $opt in
+   "a"|"A")
+      make_available
+      ;;
+   "u"|"U")
+      make_unavailable
+      ;;
+   "x"|"X")
+      exit 1
+      ;;
+   *)
+      echo "Unknown error!" >&2
+      ;;
+esac
 
-sudo losetup -d /dev/loop0
-echo "Undone!"
+make_available(){
+   if [ $# -lt 1 ]; then
+      echo "Not enough parameters. Usage: $0 path/to/file/filename.ext"
+      exit 1
+   fi
+
+   if [[ ! -e $1 || ! -r $1 || ! -f $1 ]]; then
+      echo "File $1 not found or not ready!"
+      exit 1
+   fi
+
+   sudo losetup /dev/loop0 $1
+   if [[ $? -ne 0 ]]; then
+      echo "Error with $1!" >&2
+      exit 1
+   fi
+
+   sudo tcplay -m $1 -d /dev/loop0
+   if [[ $? -ne 0 ]]; then
+      echo "Error attaching $1 container to /dev/loop0!" >&2
+      exit 1
+   fi
+}
+
+make_unavailable(){
+   sudo dmsetup remove $1
+   sudo losetup -d /dev/loop0
+   echo "Undone!"
+}
